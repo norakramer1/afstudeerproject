22edb9e394a93f49c858fd69be42cbc154b020b4
nora kramer <74241562+norakramer1@users.noreply.github.com>
2023-11-01 16:29:14 +0100

    routing wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 // import Title from './comps/Title';
 // import { collection, getDocs } from 'firebase/firestore';
 import Events from './comps/Events';
+import Login from './comps/Login';
+import Signup from './comps/Signup';
+import { Routes, Route } from 'react-router-dom';
 
  function App() {
 
   return (
     <div className="App">
-   <Events/>
+
+   <Routes>
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/" element={<Events/>} />
+   </Routes>
     </div>
   );
 }
