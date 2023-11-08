da928463151729a9ba3b5ad3be40f0d02983ad32
nora kramer <74241562+norakramer1@users.noreply.github.com>
2023-11-08 16:34:16 +0100

    extracted user interests and event interest

--- src/comps/Events.js
+++ src/comps/Events.js
@@ -2,18 +2,44 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import { db } from '../firebase/config';
 import { collection, getDocs } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 
 function Events() {
   const [events, setEvents] = useState([]);
+  const [users, setUsers] = useState([]);
   const colRef = collection(db, 'events');
+  const userRef = collection(db, 'users');
+
+  const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log('users:', users, 'events:', events)
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+   // const uid = user.uid;
+    // ...
+ } else {
+    // User is signed out
+    // ...
+  }
+});
+
+useEffect(() => {
+  const getUsers = async () => {
+    const userData = await getDocs(userRef);
+    setUsers(userData.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    
+  };
+getUsers();
+}, []);
+
   
 useEffect(() => {
-
-  // check if the interest of the logged in user matches the interest of the event
-
   const getEvents = async () => {
     const data = await getDocs(colRef);
     setEvents(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+  
   };
 getEvents();
 }, []);
